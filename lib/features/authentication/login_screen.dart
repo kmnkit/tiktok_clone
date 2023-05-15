cf1587233fe91850a70d210def2a6d29f588f921
Sungwoong Kang <k900327s@gmail.com>
2023-05-15 15:57:14 +0900

    login screen

---
+++ lib/features/authentication/login_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class LogInScreen extends StatelessWidget {
+  const LogInScreen({super.key});
+
+  void onSignupTap(BuildContext context) => Navigator.of(context).pop();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: Sizes.size40,
+          ),
+          child: Column(
+            children: [
+              Gaps.v80,
+              Text(
+                'Log in to TikTok',
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Gaps.v20,
+              Text(
+                "Manage your account, check notifications, comment on videos, and more.",
+                style: TextStyle(
+                  fontSize: Sizes.size16,
+                  color: Colors.black45,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.grey.shade100,
+        elevation: 2,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size20,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Don't have an account?",
+              ),
+              Gaps.h5,
+              GestureDetector(
+                onTap: () => onSignupTap(context),
+                child: Text(
+                  'Sign up',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
