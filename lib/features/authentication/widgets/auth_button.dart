b69770c0f1f53db59bb4cb809906c63cd13a13d0
Sungwoong Kang <k900327s@gmail.com>
2023-05-15 15:58:22 +0900

    auth button base

---
+++ lib/features/authentication/widgets/auth_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class AuthButton extends StatelessWidget {
+  final String text;
+
+  const AuthButton({
+    super.key,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      // 부모 크기에 비례해서 크기 정하게 해주는 위젯
+      widthFactor: 1,
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: Sizes.size14,
+        ),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.grey.shade300,
+            width: Sizes.size1,
+          ),
+        ),
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontSize: Sizes.size16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
