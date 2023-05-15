06e8453af0620e932a964e8d7317df6695cc7cbb
Sungwoong Kang <k900327s@gmail.com>
2023-05-15 19:31:30 +0900

    extract widgets까지

--- lib/features/authentication/username_screen.dart
+++ lib/features/authentication/username_screen.dart
@@ -76,30 +76,46 @@ class _UsernameScreenState extends State<UsernameScreen> {
               ),
             ),
             Gaps.v16,
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: AnimatedContainer(
-                padding: const EdgeInsets.symmetric(
-                  vertical: Sizes.size16,
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: _username.isEmpty
-                      ? Colors.grey.shade400
-                      : Theme.of(context).primaryColor,
-                ),
-                duration: const Duration(milliseconds: 300),
-                child: const Text(
-                  'Next',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
+            FormButton(disabled: _username.isEmpty),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class FormButton extends StatelessWidget {
+  const FormButton({
+    super.key,
+    required this.disabled,
+  });
+
+  final bool disabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: AnimatedContainer(
+        padding: const EdgeInsets.symmetric(
+          vertical: Sizes.size16,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color:
+              disabled ? Colors.grey.shade400 : Theme.of(context).primaryColor,
+        ),
+        duration: const Duration(milliseconds: 300),
+        child: AnimatedDefaultTextStyle(
+          duration: const Duration(milliseconds: 300),
+          style: TextStyle(
+            color: disabled ? Colors.grey.shade400 : Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+          child: const Text(
+            'Next',
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
