36542a2b5d26223ee4cae40e5eaad123d3adf843
Sungwoong Kang <k900327s@gmail.com>
2023-05-15 16:42:39 +0900

    auth button

--- lib/features/authentication/widgets/auth_button.dart
+++ lib/features/authentication/widgets/auth_button.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class AuthButton extends StatelessWidget {
+  final FaIcon icon;
   final String text;
 
   const AuthButton({
     super.key,
+    required this.icon,
     required this.text,
   });
 
@@ -15,22 +18,31 @@ class AuthButton extends StatelessWidget {
       // 부모 크기에 비례해서 크기 정하게 해주는 위젯
       widthFactor: 1,
       child: Container(
-        padding: const EdgeInsets.symmetric(
-          vertical: Sizes.size14,
-        ),
+        padding: const EdgeInsets.all(Sizes.size14),
         decoration: BoxDecoration(
           border: Border.all(
             color: Colors.grey.shade300,
             width: Sizes.size1,
           ),
         ),
-        child: Text(
-          text,
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-            fontSize: Sizes.size16,
-            fontWeight: FontWeight.w600,
-          ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: icon,
+            ),
+            Expanded(
+              child: Text(
+                text,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: Sizes.size16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
