35f960fecf3df6a936ed38cd3cbe28de61049a77
Sungwoong Kang <k900327s@gmail.com>
2023-05-22 12:57:02 +0900

    timeline

--- lib/features/videos/screens/timeline.dart
+++ lib/features/videos/screens/timeline.dart
@@ -9,8 +9,11 @@ class VideoTimelineScreen extends StatefulWidget {
 
 class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
   int _itemCount = 4;
+  final PageController _pageController = PageController();
 
   void _onPageChanged(int page) {
+    _pageController.animateToPage(page,
+        duration: const Duration(milliseconds: 150), curve: Curves.linear);
     if (page == _itemCount - 1) {
       _itemCount = _itemCount + 4;
       colors.addAll([
@@ -33,6 +36,7 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
+      controller: _pageController,
       itemBuilder: (context, index) => Container(
         color: colors[index],
         child: Center(
