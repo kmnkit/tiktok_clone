cdf4704171feb17f3be72ec56e8684af2e41bc11
Sungwoong Kang <k900327s@gmail.com>
2023-05-24 14:38:05 +0900

    comments section

---
+++ lib/features/videos/widgets/video_comments.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class VideoComments extends StatefulWidget {
+  const VideoComments({super.key});
+
+  @override
+  State<VideoComments> createState() => _VideoCommentsState();
+}
+
+class _VideoCommentsState extends State<VideoComments> {
+  bool _isWriting = false;
+  final ScrollController _scrollController = ScrollController();
+  void _onClosePressed() {
+    Navigator.of(context).pop();
+  }
+
+  void _stopCommenting() {
+    FocusScope.of(context).unfocus();
+    setState(() {
+      _isWriting = false;
+    });
+  }
+
+  void _onStartWriting() {
+    setState(() {
+      _isWriting = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height * 0.75,
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          Sizes.size14,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.grey.shade50,
+        appBar: AppBar(
+          backgroundColor: Colors.grey.shade50,
+          actions: [
+            IconButton(
+              onPressed: _onClosePressed,
+              icon: const FaIcon(
+                FontAwesomeIcons.xmark,
+              ),
+            ),
+          ],
+          automaticallyImplyLeading: false,
+          title: const Text(
+            "33333 comments",
+          ),
+        ),
+        body: GestureDetector(
+          onTap: _stopCommenting,
+          child: Stack(
+            children: [
+              Scrollbar(
+                controller: _scrollController,
+                child: ListView.separated(
+                  controller: _scrollController,
+                  separatorBuilder: (context, index) => Gaps.v20,
+                  padding: const EdgeInsets.only(
+                    top: Sizes.size10,
+                    bottom: Sizes.size96 + Sizes.size20,
+                    left: Sizes.size16,
+                    right: Sizes.size16,
+                  ),
+                  itemBuilder: (context, index) => Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const CircleAvatar(
+                        radius: 18,
+                        child: Text("니꼬"),
+                      ),
+                      Gaps.h10,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "So Sleepy",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: Sizes.size14,
+                                color: Colors.grey.shade500,
+                              ),
+                            ),
+                            Gaps.v3,
+                            const Text("We are done!"),
+                          ],
+                        ),
+                      ),
+                      Gaps.h10,
+                      Column(
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.heart,
+                            size: Sizes.size20,
+                            color: Colors.grey.shade500,
+                          ),
+                          Text(
+                            '52.2K',
+                            style: TextStyle(
+                              color: Colors.grey.shade500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  itemCount: 10,
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                width: size.width,
+                child: BottomAppBar(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: Sizes.size16,
+                    vertical: Sizes.size10,
+                  ),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 18,
+                        backgroundColor: Colors.grey.shade500,
+                        foregroundColor: Colors.white,
+                        child: const Text("니꼬"),
+                      ),
+                      Gaps.h10,
+                      Expanded(
+                        child: SizedBox(
+                          height: Sizes.size44,
+                          child: TextField(
+                            onTap: _onStartWriting,
+                            minLines: null,
+                            maxLines: null,
+                            expands: true,
+                            textInputAction: TextInputAction.newline,
+                            cursorColor: Theme.of(context).primaryColor,
+                            decoration: InputDecoration(
+                              hintText: "Write a comment...",
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: Sizes.size12,
+                                vertical: Sizes.size10,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(
+                                  Sizes.size12,
+                                ),
+                                borderSide: BorderSide.none,
+                              ),
+                              filled: true,
+                              fillColor: Colors.grey.shade200,
+                              suffixIcon: Padding(
+                                padding: const EdgeInsets.only(
+                                  right: Sizes.size14,
+                                ),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    FaIcon(
+                                      FontAwesomeIcons.at,
+                                      color: Colors.grey.shade700,
+                                    ),
+                                    Gaps.h10,
+                                    FaIcon(
+                                      FontAwesomeIcons.gift,
+                                      color: Colors.grey.shade700,
+                                    ),
+                                    Gaps.h10,
+                                    FaIcon(
+                                      FontAwesomeIcons.faceSmile,
+                                      color: Colors.grey.shade700,
+                                    ),
+                                    Gaps.h10,
+                                    if (_isWriting)
+                                      GestureDetector(
+                                        onTap: _stopCommenting,
+                                        child: FaIcon(
+                                          FontAwesomeIcons.circleArrowUp,
+                                          color: Theme.of(context).primaryColor,
+                                        ),
+                                      ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
