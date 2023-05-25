2ad3e0b43e77fed2c7318f91a10fd7698218e433
Sungwoong Kang <k900327s@gmail.com>
2023-05-25 09:41:58 +0900

    grid item

--- lib/features/discover/screens/discover.dart
+++ lib/features/discover/screens/discover.dart
@@ -47,7 +47,25 @@ class DiscoverScreen extends StatelessWidget {
         ),
         body: TabBarView(
           children: [
-            for (var tab in tabs)
+            GridView.builder(
+              padding: const EdgeInsets.all(
+                Sizes.size6,
+              ),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: Sizes.size10,
+                mainAxisSpacing: Sizes.size10,
+                childAspectRatio: 9 / 16,
+              ),
+              itemBuilder: (context, index) => Container(
+                color: Colors.amber,
+                child: Center(
+                  child: Text("$index"),
+                ),
+              ),
+              itemCount: 20,
+            ),
+            for (var tab in tabs.skip(1))
               Center(
                 child: Text(
                   tab,
