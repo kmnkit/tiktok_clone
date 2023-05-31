1b080deaced8d359dba22b23fd06f8b7ddb01b44
Sungwoong Kang <k900327s@gmail.com>
2023-05-31 10:40:35 +0900

    Animated List

--- lib/features/inbox/screens/chats.dart
+++ lib/features/inbox/screens/chats.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/inbox/screens/chat_detail.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -10,13 +11,78 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+  final Duration _duration = const Duration(milliseconds: 250);
+
+  void _addItem() {
+    _key.currentState?.insertItem(_items.length, duration: _duration);
+    _items.add(_items.length);
+  }
+
+  void _deleteItem(int index) {
+    _key.currentState?.removeItem(
+      index,
+      (context, animation) => SizeTransition(
+        sizeFactor: animation,
+        child: Container(
+          color: Colors.red,
+          child: _makeTile(index),
+        ),
+      ),
+      duration: _duration,
+    );
+    _items.removeAt(index);
+  }
+
+  void _onChatTap() => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => const ChatDetailScreen(),
+        ),
+      );
+
+  ListTile _makeTile(int index) {
+    return ListTile(
+      key: UniqueKey(),
+      onTap: _onChatTap,
+      onLongPress: () => _deleteItem(index),
+      leading: const CircleAvatar(
+        radius: 20,
+        foregroundImage: NetworkImage(
+          "https://avatars.githubusercontent.com/u/3612017",
+        ),
+      ),
+      title: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Nico ($index)',
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            "9:38 AM",
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size14,
+            ),
+          )
+        ],
+      ),
+      subtitle: const Text("Don't forget to study!"),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addItem,
             icon: const FaIcon(
               FontAwesomeIcons.plus,
             ),
@@ -27,40 +93,24 @@ class _ChatScreenState extends State<ChatScreen> {
           'Direct Messages',
         ),
       ),
-      body: ListView(
+      body: AnimatedList(
+        key: _key,
         padding: const EdgeInsets.symmetric(
           vertical: Sizes.size10,
         ),
-        children: [
-          ListTile(
-            leading: const CircleAvatar(
-              radius: 20,
-              foregroundImage: NetworkImage(
-                "https://avatars.githubusercontent.com/u/3612017",
-              ),
+        itemBuilder:
+            (BuildContext context, int index, Animation<double> animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            // child: ScaleTransition(
+            //   scale: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: _makeTile(index),
             ),
-            title: Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Nico',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Text(
-                  "9:38 AM",
-                  style: TextStyle(
-                    color: Colors.grey.shade500,
-                    fontSize: Sizes.size14,
-                  ),
-                )
-              ],
-            ),
-            subtitle: const Text("Don't forget to study!"),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
