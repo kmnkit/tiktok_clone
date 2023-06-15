611ef1f5b19064ca74439309600faae889b43f2a
Sungwoong Kang <k900327s@gmail.com>
2023-06-15 16:14:15 +0900

    datetime picker

--- lib/features/settings/screens/settings.dart
+++ lib/features/settings/screens/settings.dart
@@ -12,18 +12,45 @@ class SettingsScreen extends StatelessWidget {
       body: ListView(
         children: [
           ListTile(
-            onTap: () => showAboutDialog(
-              context: context,
-              applicationVersion: "1.0",
-              applicationLegalese: "All right reserved. Please don't copy me",
-            ),
+            onTap: () async {
+              // final date = await showDatePicker(
+              //   context: context,
+              //   initialDate: DateTime.now(),
+              //   firstDate: DateTime(1989),
+              //   lastDate: DateTime(2030),
+              // );
+              // print(date);
+              // final time = await showTimePicker(
+              //   context: context,
+              //   initialTime: TimeOfDay.now(),
+              // );
+              // print(time);
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(
+                  DateTime.now().year,
+                ),
+                lastDate: DateTime(2030),
+                builder: (context, child) {
+                  return Theme(
+                    data: ThemeData(
+                      appBarTheme: const AppBarTheme(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black,
+                      ),
+                    ),
+                    child: child!,
+                  );
+                },
+              );
+              print(booking);
+            },
             title: const Text(
-              "About",
+              "When is your Birthday?",
               style: TextStyle(
                 fontWeight: FontWeight.w600,
               ),
             ),
-            subtitle: const Text('About this app...'),
           ),
           const AboutListTile(),
         ],
