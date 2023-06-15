d6cc6ccbba58a7a12022c6737596716d79159949
Sungwoong Kang <k900327s@gmail.com>
2023-06-15 16:49:03 +0900

    switch list tiles

--- lib/features/settings/screens/settings.dart
+++ lib/features/settings/screens/settings.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notifications = false;
+
+  void _onNotificationChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notifications = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +25,26 @@ class SettingsScreen extends StatelessWidget {
       ),
       body: ListView(
         children: [
+          // CupertinoSwitch(
+          //   value: _notifications,
+          //   onChanged: _onNotificationChanged,
+          // ),
+          SwitchListTile.adaptive(
+              value: _notifications,
+              onChanged: _onNotificationChanged,
+              title: const Text("Enable Notifications")),
+          // Checkbox(
+          //   value: _notifications,
+          //   onChanged: _onNotificationChanged,
+          // ),
+          CheckboxListTile(
+            activeColor: Colors.black,
+            value: _notifications,
+            onChanged: _onNotificationChanged,
+            title: const Text(
+              "Enable Notifications",
+            ),
+          ),
           ListTile(
             onTap: () async {
               // final date = await showDatePicker(
